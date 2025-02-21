05d8ba2e59aae8de24e343a289baa50a13635242
Asker <kotsevasker0@gmail.com>
2025-02-21 11:35:40 +0300

    create new module and add libs

---
+++ config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+
+type(
+	AppConfig struct{
+		DB DataBaseConfig
+	}
+
+	DataBaseConfig struct{
+		DNS string
+	}
+)
+
+func NewConfig()*AppConfig{
+	 if err := godotenv.Load(".env");err != nil{
+		fmt.Println(err.Error())
+	 }
+	return &AppConfig{
+		DB: DataBaseConfig{
+			DNS: os.Getenv("DNS"),
+		},
+	}
+}
